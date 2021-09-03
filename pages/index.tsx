03fcf9375301b4aba37467f57a04aab6068c05ca
Pedro Paiao <pedroUFMS2019@gmail.com>
2021-09-03 19:42:00 -0400

    [BUG] Windows

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import React from 'react';
+
+const Home: React.FC = () => {
   return (
     <main>
       <h1>Hello Word</h1>
     </main>
   )
 }
+
+export default Home;
