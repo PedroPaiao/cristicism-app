0b6c8fd869d668441286ba0de6d739e7fe335af9
Pedro Paiao <pedroUFMS2019@gmail.com>
2021-09-03 20:15:51 -0400

    [FINISH] Patterns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import { MovieCardStyle } from '../styles/components/MovieCardStyle';
+
 const Home: React.FC = () => {
   return (
     <main>
+      <MovieCardStyle>
+        <h1>
+          Title
+        </h1>
+      </MovieCardStyle>
       <h1>Hello Word</h1>
     </main>
   )
