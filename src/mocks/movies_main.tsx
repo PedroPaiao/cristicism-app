f8b1bf4579cb6f0ee9ae5e4d38bca4a407240607
Amanda Vieira <amandavc27@gmail.com>
2021-09-18 19:11:26 -0400

    [ADD] More infos into Mocks

--- src/mocks/movies_main.tsx
+++ src/mocks/movies_main.tsx
@@ -3,6 +3,9 @@ interface MovieProps {
   title: string
   description: string
   imageLink: string
+  trailerLink: string
+  year?: number
+  director?: string
   category?: string
 }
 
@@ -21,22 +24,33 @@ export const moviesMainJson: MovieJson = {
       title: 'Viuva Negra',
       description: 'Coringa fazendo coringuisses',
       category: 'Ficção',
+      trailerLink: 'https://www.youtube.com/embed/Gm3o0bfGP3g',
+      year: 2021,
+      director: 'Cate Shortland',
       imageLink:
         'https://img.elo7.com.br/product/original/2A9E2DD/poster-cartaz-filme-coringa-2019-lancamento.jpg'
     },
     {
       id: 75,
       title: 'Doutor Estranho',
-      description: 'Coringa fazendo coringuisses',
+      description:
+        'Após sua carreira ser destruída, um brilhante, porém arrogante, cirurgião ganha uma nova chance em sua vida quando um feiticeiro o treina para se tornar o Mago Supremo.',
       category: 'Ficção',
+      trailerLink: 'https://www.youtube.com/embed/Gm3o0bfGP3g',
+      year: 2016,
+      director: 'Scott Derrickson',
       imageLink:
         'https://img.elo7.com.br/product/original/2A9E2DD/poster-cartaz-filme-coringa-2019-lancamento.jpg'
     },
     {
       id: 76,
-      title: 'Velozes e furiosos',
-      description: 'Coringa fazendo coringuisses',
+      title: 'Velozes e furiosos 8',
+      description:
+        'Depois que Brian e Mia se aposentaram, e o resto da equipe foi exonerado, Dom e Letty estão em lua de mel e levam uma vida pacata e completamente normal.',
       category: 'Ficção',
+      trailerLink: 'https://www.youtube.com/embed/KvSlvtPnZTo',
+      year: 2017,
+      director: 'F. Gary Gray',
       imageLink:
         'https://img.elo7.com.br/product/original/2A9E2DD/poster-cartaz-filme-coringa-2019-lancamento.jpg'
     }
